d63dc178ad5c033c7f68a57763c288554af8eb0b
tangmingheng <mhtang@thoughtworks.com>
2020-02-25 22:14:20 +0800

    查询文章类别api并创建路由守卫

--- app/controller/admin/main.js
+++ app/controller/admin/main.js
@@ -27,6 +27,12 @@ class MainController extends Controller {
         }
     }
 
+    //后台文章分类信息
+    async getTypeInfo() {
+        const resType = await this.app.mysql.select('type')
+        this.ctx.body = { data: resType }
+    }
+
 }
 
 module.exports = MainController
