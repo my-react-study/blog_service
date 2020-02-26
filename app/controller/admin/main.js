29d8b75afe26f83cbc9238122f1545b9ec891318
tangmingheng <mhtang@thoughtworks.com>
2020-02-26 22:23:26 +0800

    修改接口实现

--- app/controller/admin/main.js
+++ app/controller/admin/main.js
@@ -82,6 +82,24 @@ class MainController extends Controller {
         this.ctx.body = { data: res }
     }
 
+    //根据文章ID得到文章详情，用于修改文章
+    async getArticleById() {
+        let id = this.ctx.params.id
+
+        let sql = 'SELECT article.id as id,' +
+            'article.title as title,' +
+            'article.introduce as introduce,' +
+            'article.article_content as article_content,' +
+            "FROM_UNIXTIME(article.addTime,'%Y-%m-%d' ) as addTime," +
+            'article.view_count as view_count ,' +
+            'type.typeName as typeName ,' +
+            'type.id as typeId ' +
+            'FROM article LEFT JOIN type ON article.type_id = type.Id ' +
+            'WHERE article.id=' + id
+        const result = await this.app.mysql.query(sql)
+        this.ctx.body = { data: result }
+    }
+
 }
 
 module.exports = MainController
